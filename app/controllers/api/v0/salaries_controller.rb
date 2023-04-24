b0fa93cfab226822bbc5cdccf3efc604b0e9d54d
James Taylor <jrtaylor0614@gmail.com>
2023-04-24 12:02:03 -0600

    So close, final commit

--- app/controllers/api/v0/salaries_controller.rb
+++ app/controllers/api/v0/salaries_controller.rb
@@ -1,6 +1,5 @@
 class Api::V0::SalariesController < ApplicationController
   def index
-    salaries = SalaryFacade.new(params[:location]).find_salaries
-    render json: SalarySerializer.new(salaries)
+
   end
 end
--- spec/requests/api/v0/salaries_request_spec.rb
+++ spec/requests/api/v0/salaries_request_spec.rb
@@ -1,10 +1,13 @@
 require "rails_helper"
 
-Rspec.describe "Salaries API" do
+RSpec.describe "Salaries API" do
   describe "GET /api/v1/salaries" do
     it "can get salaries and forcast for a givin city" do
       VCR.use_cassette("salaries_request") do
-        get "/api/v1/salaries?location=denver,co", headers: { 'Content-Type': 'application/json', 'Accept': 'application/json' }
+        get "/api/v0/salaries?location=denver,co", headers: { 'Content-Type': 'application/json', 'Accept': 'application/json' }
+
+        response = JSON.parse(response.body, symbolize_names: true)
+        expect(response).to be_successful
       end
     end
   end

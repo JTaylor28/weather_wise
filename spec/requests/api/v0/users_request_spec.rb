98a8840c9d2536d46d2562243e02eb563458ffff
James Taylor <jrtaylor0614@gmail.com>
2023-04-24 20:41:22 -0600

    Test: User request happy/sad path

--- spec/requests/api/v0/users_request_spec.rb
+++ spec/requests/api/v0/users_request_spec.rb
@@ -3,27 +3,66 @@ require "rails_helper"
 RSpec.describe "/api/vo/users", type: :request do
   describe "POST /create" do
     context "When successful" do
-      let(:user_params) { { email: "email@email.com", password: "goodpassword", password_confirmation: "goodpassword" } }
-
       it "creates a new user" do
+        user_params = { email: "email@email.com", password: "goodpassword", password_confirmation: "goodpassword"}
         headers = { "CONTENT_TYPE" => "application/json" }
         post "/api/v0/users", headers: headers, params: JSON.generate(user_params)
 
         expect(response).to have_http_status(201)
 
         parsed = JSON.parse(response.body, symbolize_names: true)
-
         expect(parsed).to be_a(Hash)
         expect(parsed[:data]).to be_a(Hash)
         expect(parsed[:data].keys).to eq([:id, :type, :attributes])
-
         expect(parsed[:data][:id]).to be_a(String)
-        expect(parsed[:data][:type]).to be_a("users")
+        expect(parsed[:data][:type]).to eq("users")
         expect(parsed[:data][:attributes]).to be_a(Hash)
         expect(parsed[:data][:attributes].keys).to eq([:email, :api_key])
-        expect(parsed[:data][:attributes][:email]).to be_a("email@email.com")
+        expect(parsed[:data][:attributes][:email]).to eq("email@email.com")
         expect(parsed[:data][:attributes][:api_key]).to be_a(String)
+        require 'pry'; binding.pry
       end
     end
+
+    context "When unsuccessful" do
+      it "returns an error if email is missing" do
+        user_params = { email: " ", password: "goodpassword", password_confirmation: "goodpassword" }
+        headers = { "CONTENT_TYPE" => "application/json" }
+        post "/api/v0/users", headers: headers, params: JSON.generate(user_params)
+
+        expect(response).to have_http_status(404)
+
+        parsed = JSON.parse(response.body, symbolize_names: true)
+        expect(parsed).to be_a(Hash)
+        expect(parsed[:errors].first[:detail]).to eq(["invalid credentials"])
+        expect(parsed[:errors].first[:title]).to eq("Bad Request")
+      end
+
+      it "returns an error if password is missing" do
+        user_params = { email: "email@email.com", password: "", password_confirmation: ""}
+        headers = { "CONTENT_TYPE" => "application/json" }
+        post "/api/v0/users", headers: headers, params: JSON.generate(user_params)
+
+        expect(response).to have_http_status(404)
+        
+        parsed = JSON.parse(response.body, symbolize_names: true)
+        expect(parsed).to be_a(Hash)
+        expect(parsed[:errors].first[:detail]).to eq(["invalid credentials"])
+        expect(parsed[:errors].first[:title]).to eq("Bad Request")
+      end
+
+      it "returns an error if password_confirmation does not match password" do
+        user_params = { email: "email@email.com ", password: "password", password_confirmation: "wordpass" }
+        headers = { "CONTENT_TYPE" => "application/json" }
+        post "/api/v0/users", headers: headers, params: JSON.generate(user_params)
+
+        expect(response).to have_http_status(404)
+
+        parsed = JSON.parse(response.body, symbolize_names: true)
+        expect(parsed).to be_a(Hash)
+        expect(parsed[:errors].first[:detail]).to eq(["invalid credentials"])
+        expect(parsed[:errors].first[:title]).to eq("Bad Request")
+      end
+    end 
   end
 end

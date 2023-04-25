c26d1db6384e64911c47511892b54c3b99d7e556
James R Taylor <117054959+JTaylor28@users.noreply.github.com>
2023-04-24 21:01:52 -0600

    User story 2 (#2)
    * Add gems
    
    * Test: Mapquest Service
    
    * Feat: Mapquest Service
    
    * Test: Mapquest Facade #find_coordinates
    
    * Feat: mapquest facade #find_coordinates
    
    * Test: Weather Service
    
    * Feat: Weather service
    
    * Test: weather fasade
    
    * Feat: weather facade #find_five_day_weather
    
    * Test: weather poro
    
    * Feat: Weather poro
    
    * Feat: forecast index
    
    * Test: forecast request
    
    * Fix: naming error
    
    * Add forecast serilizer
    
    * Add forecast controller
    
    * Add cassettes
    
    * Feat: Create user table
    
    * Test: user modle
    
    * Feat: user model
    
    * Feat: add rake task to fix server issue
    
    * Test: user request
    
    * Feat: users create route
    
    * Test: User request happy/sad path
    
    * Feat: user_controller #Create
    
    * Feat: Error_serializer
    
    * Feat: Users serializer

---
+++ app/controllers/api/v0/users_controller.rb
@@ -0,0 +1,36 @@
+class Api::V0::UsersController < ApplicationController
+  before_action :check_for_nil_params, only: [:create]
+
+  def create
+    new_params = user_params
+    new_params[:email] = new_params[:email].downcase
+    new_user = User.new(email: new_params[:email], password: params[:password])  
+
+    if new_user.save
+      api_key = generate_api_key(new_user)
+      new_user.update(api_key: api_key)
+      render json: UsersSerializer.new(new_user), status: 201
+    else
+      render json: ErrorSerializer.new("invalid credentials").bad_request, status: 404
+    end
+  end
+
+  private
+
+  def check_for_nil_params
+    if params[:email].nil? || params[:password].nil? || params[:password_confirmation].nil? || params[:password] != params[:password_confirmation]
+      render json: ErrorSerializer.new("invalid credentials").bad_request, status: 404
+    end
+  end
+
+  def generate_api_key(user)
+    loop do
+      api_key = SecureRandom.hex(16)
+      break api_key unless User.exists?(api_key: api_key)
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
+end
---
+++ spec/requests/api/v0/users_request_spec.rb
@@ -0,0 +1,67 @@
+require "rails_helper"
+
+RSpec.describe "/api/vo/users", type: :request do
+  describe "POST /create" do
+    context "When successful" do
+      it "creates a new user" do
+        user_params = { email: "email@email.com", password: "goodpassword", password_confirmation: "goodpassword"}
+        headers = { "CONTENT_TYPE" => "application/json" }
+        post "/api/v0/users", headers: headers, params: JSON.generate(user_params)
+
+        expect(response).to have_http_status(201)
+
+        parsed = JSON.parse(response.body, symbolize_names: true)
+        expect(parsed).to be_a(Hash)
+        expect(parsed[:data]).to be_a(Hash)
+        expect(parsed[:data].keys).to eq([:id, :type, :attributes])
+        expect(parsed[:data][:id]).to be_a(String)
+        expect(parsed[:data][:type]).to eq("users")
+        expect(parsed[:data][:attributes]).to be_a(Hash)
+        expect(parsed[:data][:attributes].keys).to eq([:email, :api_key])
+        expect(parsed[:data][:attributes][:email]).to eq("email@email.com")
+        expect(parsed[:data][:attributes][:api_key]).to be_a(String)
+      end
+    end
+
+    context "When unsuccessful" do
+      it "returns an error if email is missing" do
+        user_params = { email: " ", password: "goodpassword", password_confirmation: "goodpassword" }
+        headers = { "CONTENT_TYPE" => "application/json" }
+        post "/api/v0/users", headers: headers, params: JSON.generate(user_params)
+
+        expect(response).to have_http_status(404)
+
+        parsed = JSON.parse(response.body, symbolize_names: true)
+        expect(parsed).to be_a(Hash)
+        expect(parsed[:errors].first[:detail]).to eq(["invalid credentials"])
+        expect(parsed[:errors].first[:title]).to eq("Bad Request")
+      end
+
+      it "returns an error if password is missing" do
+        user_params = { email: "email@email.com", password: "", password_confirmation: ""}
+        headers = { "CONTENT_TYPE" => "application/json" }
+        post "/api/v0/users", headers: headers, params: JSON.generate(user_params)
+
+        expect(response).to have_http_status(404)
+        
+        parsed = JSON.parse(response.body, symbolize_names: true)
+        expect(parsed).to be_a(Hash)
+        expect(parsed[:errors].first[:detail]).to eq(["invalid credentials"])
+        expect(parsed[:errors].first[:title]).to eq("Bad Request")
+      end
+
+      it "returns an error if password_confirmation does not match password" do
+        user_params = { email: "email@email.com ", password: "password", password_confirmation: "wordpass" }
+        headers = { "CONTENT_TYPE" => "application/json" }
+        post "/api/v0/users", headers: headers, params: JSON.generate(user_params)
+
+        expect(response).to have_http_status(404)
+
+        parsed = JSON.parse(response.body, symbolize_names: true)
+        expect(parsed).to be_a(Hash)
+        expect(parsed[:errors].first[:detail]).to eq(["invalid credentials"])
+        expect(parsed[:errors].first[:title]).to eq("Bad Request")
+      end
+    end 
+  end
+end

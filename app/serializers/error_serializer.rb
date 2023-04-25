22a56a5a71448839b185a54dbac118bcf297b02b
James Taylor <jrtaylor0614@gmail.com>
2023-04-25 00:07:04 -0600

    Feat: login serializer

--- app/serializers/error_serializer.rb
+++ app/serializers/error_serializer.rb
@@ -3,12 +3,24 @@ class ErrorSerializer
     @errors = errors
   end
 
-  def bad_request
+  def invalid_request
     {
       errors: [
         {
           "status": "404",
           "title": "Bad Request",
+          "detail": [@errors]
+        }
+      ]
+    }
+  end
+
+  def bad_credentials
+    {
+      errors: [
+        {
+          "status": "401",
+          "title": "Unauthorized",
           "detail": [@errors]
         }
       ]
